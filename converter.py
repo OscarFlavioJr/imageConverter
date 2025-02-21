a8fbedfc7517ae741b05c15c008b7240627d93bd
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2025-02-21 11:42:51 -0300

    PNG CONVERSOR!

--- converter.py
+++ converter.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 for infile in sys.argv[1:]:
     f , e = os.path.splitext(infile)
-    outfile = f + ".pdf"
+    outfile = f + ".png"
     if infile != outfile:
         try:
             with Image.open(infile) as im:
